00d2aa1f64f2348505a9763a0202d10ceb328abc
j385furu <j520385k@mails.cc.ehime-u.ac.jp>
2024-06-25 14:01:14 +0900

    Update 1.c

--- 1.c
+++ 1.c
@@ -7,7 +7,7 @@ int main(void)
   printf("n = ");
   scanf("%d", &n);
 
-  if( n & 1 == 0 ){
+  if( (n & 1) == 0 ){ //括弧を使用して優先順位を明示するため
     printf("%d は偶数です\n", n);
   }else{
     printf("%d は奇数です\n", n);
