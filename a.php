376ba4b3c4e9fd163c10d7169847e904289ce0e0
j385furu <j520385k@mails.cc.ehime-u.ac.jp>
2024-06-25 14:45:23 +0900

    Update a.php

--- a.php
+++ a.php
@@ -2,7 +2,7 @@
 <?php 
   foreach(file('/usr/share/dict/words') as $line){
     // strpos() 関数を使って、文字列 "xy" が含まれる単語を echo "$line"; で表示しなさい。
-    if (strpos($line, 'abc') !== false) {
+    if (strpos($line, 'abc') !== false) {// 文字列 "abc" が含まれる単語を表示する
       echo "$line";
     }
   } 
